5f4ff18e221cac495e92a01a401e33ce625c43b4
kume1a <kumela011@gmail.com>
2024-01-19 18:41:01 +0400

    remove signup

--- internal/modules/auth/dto.go
+++ internal/modules/auth/dto.go
@@ -5,22 +5,11 @@ import (
 )
 
 type signInDTO struct {
-	Email    string `json:"email" valid:"email,stringlength(1|255)"`
-	Password string `json:"password" valid:"stringlength(6|255)"`
-}
-
-type signUpDTO struct {
-	Email    string `json:"email" valid:"email,stringlength(1|255)"`
-	Password string `json:"password" valid:"stringlength(6|255)"`
 	Name     string `json:"name" valid:"stringlength(1|255)"`
+	Password string `json:"password" valid:"stringlength(6|255)"`
 }
 
 func (dto signInDTO) Validate() error {
-	_, err := govalidator.ValidateStruct(dto)
-	return err
-}
-
-func (dto signUpDTO) Validate() error {
 	_, err := govalidator.ValidateStruct(dto)
 	return err
 }
--- internal/modules/auth/handler.go
+++ internal/modules/auth/handler.go
@@ -19,23 +19,10 @@ func signInHandler(apiCfg *shared.ApiConfg) http.HandlerFunc {
 	}
 }
 
-func signUpHandler(apiCfg *shared.ApiConfg) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		body, err := shared.ValidateRequest[signUpDTO](r)
-		if err != nil {
-			shared.ResError(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		shared.ResJson(w, http.StatusOK, body)
-	}
-}
-
 func Router(apiCfg *shared.ApiConfg, router *mux.Router) *mux.Router {
 	r := router.PathPrefix("/auth").Subrouter()
 
 	r.HandleFunc("/signIn", signInHandler(apiCfg)).Methods("POST")
-	r.HandleFunc("/signUp", signUpHandler(apiCfg)).Methods("POST")
 
 	return r
 }
